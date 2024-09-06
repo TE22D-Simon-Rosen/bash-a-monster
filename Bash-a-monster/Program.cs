6d52844f00e8cfe560450b5051af663fb2d6cb76
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2024-09-06 12:30:58 +0200

    first commit

---
+++ Bash-a-monster/Program.cs
@@ -0,0 +1,29 @@
+﻿Entity player = new();
+Entity enemy = new();
+Random random = new();
+
+player.hp = 100;
+enemy.hp = 100;
+
+player.name = "player";
+enemy.name = "enemy";
+
+void ShowStats(Entity player, Entity enemy){
+    Console.WriteLine($"\n{player.name} - {player.hp} hp  |  {enemy.name} - {enemy.hp} hp");
+    Console.ReadLine();
+}
+
+void Attack(Entity attacker, Entity target){
+    int damage = random.Next(attacker.minAttack, attacker.maxAttack);
+    Console.WriteLine($"\n{attacker} deals {damage} damage to {target}!");
+    target.hp -= damage;
+    Console.ReadLine();
+}
+
+
+while (player.hp > 0 || enemy.hp > 0){
+    ShowStats(player, enemy);
+    Attack(player, enemy);
+    ShowStats(player, enemy);
+    Attack(enemy, player);
+}
---
+++ Bash-a-monster/entity.cs
@@ -0,0 +1,6 @@
+public class Entity{
+    public int hp;
+    public int minAttack = 10;
+    public int maxAttack = 30;
+    public string name;
+}
